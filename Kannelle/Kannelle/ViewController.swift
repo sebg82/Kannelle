d832dcb161601a82abe47ccbd9850b72ac958bf2
Sebastien Gohier <>
2019-10-09 14:29:36 +0200

    Initial commit

---
+++ Kannelle/Kannelle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kannelle
+//
+//  Created by Sebastien Gohier on 09/10/2019.
+//  Copyright © 2019 Sebastien Gohier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
